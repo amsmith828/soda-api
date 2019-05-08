b6dd3b781371bb7d686bd80d27abea89ea0fc8b6
ams0828 <austin.smith@daugherty.com>
2019-05-08 11:54:04 -0500

    final class commit

--- routes/sodaRouter.js
+++ routes/sodaRouter.js
@@ -36,4 +36,36 @@ sodaRouter.get('/:soda_id', (req, res) => {
     });
 });
 
+sodaRouter.put('/:soda_id', (req, res) => {
+    Soda.findById(req.params.soda_id, (err, document) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            document.name = req.body.name
+            document.rating = req.body.rating
+
+            document.save((savedErr, savedDoc) => {
+                if (savedErr) {
+                    res.status(400).send(savedErr);
+                } else {
+                    res.send(`Soda posted!\n${savedDoc}`);
+                }
+            });
+
+        }
+    });
+});
+
+sodaRouter.delete('/:soda_id', (req, res) => {
+    Soda.deleteOne ({
+        _id: req.params.soda_id
+    }, (err) => {
+       if (err) {
+           res.status(400).send(err);
+       } else {
+           res.send(`You have successfully deleted soda: ${req.params.soda_id}`);
+       }
+    });
+});
+
 module.exports = sodaRouter;
